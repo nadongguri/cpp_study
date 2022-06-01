9b21a9a38eac364a4fc99fe94dd9584f116fd2a8
Dongjin Na <dongjin.na@samsung.com>
2022-06-01 21:56:03 +0900

    Update examples

--- src/const/const.cpp
+++ src/const/const.cpp
@@ -1,9 +1,10 @@
-//http://www.cplusplus.com/forum/general/223039/
+// http://www.cplusplus.com/forum/general/223039/
 #include <iostream>
 
 using namespace std;
 
-void testFunction() //const //error: non-member function ‘void testFunction()’ cannot have cv-qualifier
+// void testFunction() const //error: non-member function ‘void testFunction()’ cannot have cv-qualifier
+void testFunction()
 {
 	cout << "testFunction()" << endl;
 	return;
@@ -18,50 +19,70 @@ class MyClass {
 	public:
 		MyClass() {
 			//// const variables ////
-			int c0 = 96, c0_0 = 96;
-			int const c1 = 96; // ok
-			const int c2 = 96; // ok
+			int c0 = 96;
+            int c0_0 = 96;
+			int const c1 = 96;
+			const int c2 = 96;
 			// int c3 const = 96; // error: expected initializer before ‘const’
-			// const int const c4 = 96; // error: duplicate ‘const’
+
+            // Apple clang version 13.1.6 (clang-1316.0.21.2.5) warning: duplicate 'const' declaration specifier [-Wduplicate-decl-specifier]
+            // error: duplicate ‘const’
+			// const int const c4 = 96; 
 			
 			// c1 = 97; // error: assignment of read-only variable ‘c1’
 			// c2 = 97; // error: assignment of read-only variable ‘c2’
 			
 			//// const pointers ////
-			int* const p1_0 = &c0; // const for pointer address
-			//int* const p1_1 = &c1; //error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p1_2 = &c2; //error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p1_3; //error: uninitialized const ‘p1_3’ [-fpermissive]
+            // const for pointer address
+			int* const p1_0 = &c0;
+			// int* const p1_1 = &c1; //error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+			// int* const p1_2 = &c2; //error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+			// int* const p1_3; //error: uninitialized const ‘p1_3’ [-fpermissive]
 			int* const p1_3 = NULL;
-			//p1_3 = &c0_0; //error: assignment of read-only variable ‘p1_3’
-			
-			//p1_0 = &c0_0; // error: assignment of read-only variable ‘p1_0’
-			*p1_0 = 97; // Okay.
+			// p1_3 = &c0_0; //error: assignment of read-only variable ‘p1_3’
+			// p1_0 = &c0_0; // error: assignment of read-only variable ‘p1_0’
+			*p1_0 = 97;
+            *p1_0 = 100;
 
+            // const for value of address
 			const int* p2_0 = &c0; // const for value of address
 			const int* p2_1 = &c1; // const for value of address
 			const int* p2_2 = &c2; // const for value of address
-
-			p2_1 = &c0; // Okay
+			p2_1 = &c0;
 			//*p2_2 = 97; // error: assignment of read-only location ‘* p2_2’
 
 			//both
 			const int* const p3_0 = &c0;
 			const int* const p3_1 = &c1;
 			const int* const p3_2 = &c2;
+            // p3_0 = &c1; // error
+            // p3_0 = p3_1; // error
+            // *p3_0 = 100; // error
 
-			int* const p_p1_0 = p1_0;
-			//int* const p_p2_0 = p2_0; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p_p2_1 = p2_2; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p_p3_0 = p3_0; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p_p3_1 = p3_1; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
-			//int* const p_p3_2 = p3_2; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+            int* p_p1_0 = p1_0;
+            p_p1_0 = &c0;
+            // int *p_p2_0 = p2_0; // error
+            // int *p_p3_0 = p3_0; // error
+			int* const p_c_p1_0 = p1_0; // int* const p1_0
+			// int* const p_c_p2_0 = p2_0; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+			const int* c_p_p2_0 = p2_0; // const int* p2_0
+			// int* const p_c_p3_0 = p3_0; // error: invalid conversion from ‘const int*’ to ‘int*’ [-fpermissive]
+			const int* c_p_p3_0 = p3_0; // const int* const p3_0
+            c_p_p3_0 = &c0;
+            c_p_p3_0 = p3_0;
+            // c_p_p3_0 = &p3_0; // error
+            
+            const int* const c_p_c_p3_0 = p3_0; // const int* const p3_0
+            // c_p_c_p3_0 = &c0; // error
+            // c_p_c_p3_0 = p3_0; // error
+            const int* const *c_p_c_p3_1 = &p3_1;
+            const int* const *c_pp_c_p3_0 = &p3_0;
+            // const int const *c_t_c_p_p3_0 = p3_0; // warning or error, duplicate const declaration
 
 			myptr = &myprop;
-
 		};
 		// A const method can't modify class properties:
-		// int modMyprop1(const Myclass *this) so this->myprop++ 
+		// int modMyprop1(const MyClass *this) so this->myprop++ 
 		// int modMyprop1() const { return ++myprop; } 	// error: increment of member
 								// 'MyClass::myprop' in
 								// read-only object
